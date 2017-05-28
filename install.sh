6717e7fc6bc48930658ebb8eb49a2407c2ee1096
Skylar Brown <skylarmb@gmail.com>
2017-05-28 16:03:40 -0700

    install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# run from ~/dotfiles directory, not from ~/
+
+# link files
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.zprofile ~/.zprofile
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.itermprofile ~/.itermprofile
+
+# set up vim folders
+mkdir ~/.vim
+mkdir ~/.vim/tmp
+mkdir ~/.vim/tmp/swap
+mkdir ~/.vim/tmp/backup
