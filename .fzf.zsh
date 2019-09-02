044185fc577d8c386a1929551ad90b1a80356a9d
Skylar Brown <skylarmb@gmail.com>
2019-09-02 00:22:20 -0700

    wsl specific changes

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,14 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */home/skyla/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/skyla/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/skyla/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
-
+source "/home/skyla/.fzf/shell/key-bindings.zsh"
